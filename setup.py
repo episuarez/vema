615ea8524bd82c9deaf1bcc0bfe7840ef1cc3493
Epifanio Suárez Martínez <episuarez@users.noreply.github.com>
2021-08-20 00:09:56 +0200

    Fix long description

--- setup.py
+++ setup.py
@@ -2,11 +2,43 @@ import urllib.request
 
 from setuptools import setup
 
-readme = urllib.request.urlopen("https://github.com/episuarez/vema/blob/d4e18808a2b7c2b94bc6424f4a921ca621e0a4b0/readme.md").read();
+readme = """# Vema
+
+Vema is a solution for developing static web pages, from Python + Flask, which has the basic tools and already configured to focus on the design of the pages.
+
+## Features
+
+* Render pages made in HTML or MD.
+* Build static pages, so you only have to upload them to your hosting.
+* Seo optimization.
+* Routes generator.
+* Optimization of the resources of your pages.
+* Free and open-source software
+
+What feature do you miss or would you like to have?
+
+## Install
+
+Clone the repository and work with it. Or you can also install it as a package and develop it on your own. <https://pypi.org/project/vema/>
+
+To install it.
+
+```python
+pip install vema
+```
+
+## Documentation
+
+Here you can find the [project documentation](https://github.com/episuarez/vema/wiki)
+
+## First steps
+
+You can see the first steps here. [First steps](https://github.com/episuarez/vema/wiki#how-to-start)
+""";
 
 setup(
     name="vema",
-    version="0.1.3",
+    version="0.1.4",
     description="Vema is a solution for developing static web pages, from Python + Flask, which has the basic tools and already configured to focus on the design of the pages.",
     long_description=readme,
     long_description_content_type="text/markdown",
