a70e831b5a312d3f75bb8b274d08ac0eff6a06b9
Epifanio Suárez Martínez <episuarez@users.noreply.github.com>
2021-08-20 00:04:16 +0200

    Update readme pypi

--- readme.md
+++ readme.md
@@ -15,7 +15,7 @@ What feature do you miss or would you like to have?
 
 ## Install
 
-Clone the repository and work with it. Or you can also install it as a package and develop it on your own. https://pypi.org/project/vema/
+Clone the repository and work with it. Or you can also install it as a package and develop it on your own. <https://pypi.org/project/vema/>
 
 To install it.
 
--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
-from os import path
+import urllib.request
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, "readme.md"), encoding="utf-8") as file:
-    long_description = file.read()
+readme = urllib.request.urlopen("https://github.com/episuarez/vema/blob/d4e18808a2b7c2b94bc6424f4a921ca621e0a4b0/readme.md").read();
 
 setup(
     name="vema",
     version="0.1.3",
     description="Vema is a solution for developing static web pages, from Python + Flask, which has the basic tools and already configured to focus on the design of the pages.",
-    long_description=long_description,
+    long_description=readme,
     long_description_content_type="text/markdown",
-    license="MIT",
+    license="BSD",
+    platforms='any',
     author="Epifanio Suárez Martínez",
     author_email="episuarez@pm.es",
     url="https://github.com/episuarez/vema",
@@ -21,8 +20,10 @@ setup(
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: Implementation :: PyPy',
         'Development Status :: 2 - Pre-Alpha',
-        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
